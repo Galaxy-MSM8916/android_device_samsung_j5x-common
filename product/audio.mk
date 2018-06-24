75b204d74d6ed8ad973fec5cef8ea7d494f1cbbc
Vincent <vincent.zvikaramba@mail.utoronto.ca>
2018-06-24 19:14:05 -0400

    Revert "j5x-common: Copy qcom mixer paths"
    This reverts commit 78b190459f6fe678746c461900fbb931eebbf43d.
    
    Change-Id: I7beb3d214afdcd75c68b3a12216b9620678f4308

--- product/audio.mk
+++ product/audio.mk
@@ -1,9 +1,3 @@
-CONFIG_PATH := hardware/qcom/audio-caf/msm8916/configs
-
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
-
-# Mixer
-PRODUCT_COPY_FILES += \
-	$(CONFIG_PATH)/msm8916_32/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
