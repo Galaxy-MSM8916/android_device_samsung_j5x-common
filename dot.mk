5ff0cb14d1f41d20741c81614a49a79bee51091a
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-10-07 01:12:15 -0400

    j5x-common: Add dotOS Makefile
    Change-Id: Ia64a32243b197af7b190b000998d96e880206b20

---
+++ dot.mk
@@ -0,0 +1,18 @@
+# Release name
+PRODUCT_RELEASE_NAME := Samsung Galaxy J5 2016
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
+# Inherit some common Dot OS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
+$(call inherit-product, vendor/dot/config/telephony.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
