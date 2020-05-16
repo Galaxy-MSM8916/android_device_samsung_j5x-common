64335563649a7b1322613d844575c32f58a201ab
zqm64 <zqm64@outlook.com>
2020-05-16 14:36:48 +0800

    j5x-common: Append j5xltecmcc to device list

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter j5xnlte j5xlte, $(TARGET_DEVICE)),)
+ifneq ($(filter j5xnlte j5xlte j5xltecmcc, $(TARGET_DEVICE)),)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
