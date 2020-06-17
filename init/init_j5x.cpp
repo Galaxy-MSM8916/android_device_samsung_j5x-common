91c99469fb9df86b856d76cfcc2923cdc18dafd3
Gegham Zakaryan <zakaryan.2004@outlook.com>
2020-06-18 03:04:24 +0800

    j5x-common: libinit: Don't depend on libinit_msm8916
    libinit_msm8916 has been removed, and all of the necessary functions have been moved down to j5x-common.
    
    Change-Id: I781b44f52f8e411ef3a4b50fa40abd56142e3a9f

--- init/init_j5x.cpp
+++ init/init_j5x.cpp
@@ -27,9 +27,142 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <init_msm8916.h>
+#include <android-base/file.h>
+#include <android-base/strings.h>
 
-void init_target_properties(void)
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#define SIMSLOT_FILE "/proc/simslot_count"
+
+#include <android-base/properties.h>
+#include <android-base/logging.h>
+
+#include "vendor_init.h"
+#include "property_service.h"
+
+#define SERIAL_NUMBER_FILE "/efs/FactoryApp/serial_no"
+
+using android::base::GetProperty;
+using android::base::ReadFileToString;
+using android::base::Trim;
+using android::init::property_set;
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
+
+/* Read the file at filename and returns the integer
+ * value in the file.
+ *
+ * @prereq: Assumes that integer is non-negative.
+ *
+ * @return: integer value read if succesful, -1 otherwise. */
+int read_integer(const char* filename)
+{
+	int retval;
+	FILE * file;
+
+	/* open the file */
+	if (!(file = fopen(filename, "r"))) {
+		return -1;
+	}
+	/* read the value from the file */
+	fscanf(file, "%d", &retval);
+	fclose(file);
+
+	return retval;
+}
+
+void set_fingerprint()
+{
+	std::string fingerprint = GetProperty("ro.build.fingerprint", "");
+
+	if ((strlen(fingerprint.c_str()) > 1) && (strlen(fingerprint.c_str()) <= PROP_VALUE_MAX))
+		return;
+
+	char new_fingerprint[PROP_VALUE_MAX+1];
+
+	std::string build_id = GetProperty("ro.build.id","");
+	std::string build_tags = GetProperty("ro.build.tags","");
+	std::string build_type = GetProperty("ro.build.type","");
+	std::string device = GetProperty("ro.product.device","");
+	std::string incremental_version = GetProperty("ro.build.version.incremental","");
+	std::string release_version = GetProperty("ro.build.version.release","");
+
+	snprintf(new_fingerprint, PROP_VALUE_MAX, "samsung/%s/%s:%s/%s/%s:%s/%s",
+		device.c_str(), device.c_str(), release_version.c_str(), build_id.c_str(),
+		incremental_version.c_str(), build_type.c_str(), build_tags.c_str());
+
+	property_override_dual("ro.build.fingerprint", "ro.boot.fingerprint", new_fingerprint);
+}
+
+void set_dsds_properties()
+{
+	android::init::property_set("ro.multisim.simslotcount", "2");
+	android::init::property_set("ro.telephony.ril.config", "simactivation");
+	android::init::property_set("persist.radio.multisim.config", "dsds");
+	android::init::property_set("rild.libpath2", "/system/lib/libsec-ril-dsds.so");
+}
+
+void set_gsm_properties()
+{
+	android::init::property_set("telephony.lteOnCdmaDevice", "0");
+	android::init::property_set("ro.telephony.default_network", "9");
+}
+
+void set_lte_properties()
+{
+	android::init::property_set("persist.radio.lte_vrte_ltd", "1");
+	android::init::property_set("telephony.lteOnCdmaDevice", "0");
+	android::init::property_set("telephony.lteOnGsmDevice", "1");
+	android::init::property_set("ro.telephony.default_network", "10");
+}
+
+void set_target_properties(const char *device, const char *model)
+{
+	property_override_dual("ro.product.device", "ro.product.vendor.device", device);
+	property_override_dual("ro.product.model", "ro.product.vendor.model", model);
+
+	android::init::property_set("ro.ril.telephony.mqanelements", "6");
+
+	/* check and/or set fingerprint */
+	set_fingerprint();
+
+	/* check for multi-sim devices */
+
+	/* check if the simslot count file exists */
+	if (access(SIMSLOT_FILE, F_OK) == 0) {
+		int sim_count = read_integer(SIMSLOT_FILE);
+
+		/* set the dual sim props */
+		if (sim_count == 2)
+			set_dsds_properties();
+	}
+
+	char const *serial_number_file = SERIAL_NUMBER_FILE;
+	std::string serial_number;
+
+	if (ReadFileToString(serial_number_file, &serial_number)) {
+        	serial_number = Trim(serial_number);
+        	property_override("ro.serialno", serial_number.c_str());
+	}
+}
+
+void vendor_load_properties()
 {
 	char *device = NULL;
 	char *model = NULL;
